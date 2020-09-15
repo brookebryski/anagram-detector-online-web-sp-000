ce08d266d7afdaa30939fcc28e3c8dcdcdafd1df
chic-state-0895 <brookeabryski@gmail.com>
2020-09-15 19:39:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,4 +3,18 @@ class Anagram
 
    attr_accessor :word
 
+   initialize(word)
+   @word = word
+   anagram_array = []
+    end
+    
+    def match(array)
+    word_array = @word.split(" ")
+      if word_array == anagram_array 
+        puts true
+        @word << anagram_array
+      else
+        puts false
+    end
+    
 end
