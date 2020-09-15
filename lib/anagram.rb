c8667c14f6f2aade7854b352d5da3fa100d22e98
chic-state-0895 <brookeabryski@gmail.com>
2020-09-15 19:50:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,16 +5,12 @@ class Anagram
 
    def initialize(word)
    @word = word
-   anagram_array = []
     end
 
     def match(anagram_array)
-    word_array = @word.split(" ")
-      if word_array.sort == anagram_array.sort
-        puts true
-        @word << anagram_array
-      else
-        puts false
-    end
+   anagram_array.select do |word|
+     word.split("").sort == @word.split("").sort
+   end
+ end
 end
-end
+
